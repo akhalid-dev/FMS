8d9c198087c980a2ce41ae9aa5b698c0b9aac37c
Khyber Lahori <akhal022@uottawa.ca>
2020-10-31 14:35:04 -0400

    init

---
+++ src/Components/Balance/Balance.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Balance.module.scss';
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>
+                Your Balance
+            </h4>
+
+            <h1 id="balance">
+                $0.00
+            </h1>
+        </>
+    )
+}
+
---
+++ src/Components/IncomeExpenses/IncomeExpenses.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './IncomeExpenses.module.scss';
+
+export const IncomeExpenses = () => {
+    return ( 
+        <React.Fragment>
+            <div className={styles['inc-exp-container']}>
+                <div>
+                    <h4>
+                        Income
+                    </h4>
+                    <p id="money-plus" className="money plus" >
+                        +$0.00
+                    </p>
+                </div>
+
+                <div>
+                    <h4>
+                        Expense
+                    </h4>
+                    <p id="money-plus" className="money minus" >
+                        -$0.00
+                    </p>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
